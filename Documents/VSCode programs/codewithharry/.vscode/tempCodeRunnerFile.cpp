ce10779af93eb6e2540407cdf359cd4b19489370
LegioN2004 <sunnybaruasins@gmail.com>
2022-06-12 09:54:12 +0530

    added all my programs

---
+++ Documents/VSCode programs/codewithharry/.vscode/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+for (int  i = 0; i < 4; i++)
+    {
+        /* code */
+       // cout<<i<<endl;
+        if(i==2)  // if we want to stop the loop after the value is 2 then we can use this also we can use any other value
+
+        {
+           break; // this will break the loop and stop the execution of loop after the value of i is printed note that the value given in brackets will also come as output
+        }         //will also come as output if the i is printed before the break statement
+           cout<<i<<endl;   // if the cout statement is printed after the break then the 2 will not be included i.e the value of (i)
+    }
+    
---
+++ Documents/VSCode programs/codewithharry/random practices C++/just4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 32, b = 34;
+    c = a + b;
+    cout << "helllo how are you " << a << " and  " << b << " is " << c << endl;
+    return 0;
+}
