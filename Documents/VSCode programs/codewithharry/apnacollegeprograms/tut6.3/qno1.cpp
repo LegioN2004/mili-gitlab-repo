9a6dbb6555a605a4ba13b78076753594ef865b1c
LegioN2004 <sunnybaruasins@gmail.com>
2022-07-03 18:44:09 +0530

    new

--- Documents/VSCode programs/codewithharry/apnacollegeprograms/tut6.3/qno1.cpp
+++ Documents/VSCode programs/codewithharry/apnacollegeprograms/tut6.3/qno1.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
    
+int sum(int n){
+   int ans = 0;
+   for(int i= 0; i<=n; i++){
+      ans= ans+ i;
+   }
+   return ans;
+}
+
 int main(){
-   
+   int n;
+   cin>>n;
+
+   cout << sum(n) << endl;
    return 0;
 }
