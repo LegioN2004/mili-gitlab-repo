70aec8751e32ffa2b9d63812066856d38fb400fc
LegioN2004 <sunnybaruasins@gmail.com>
2022-08-03 14:15:33 +0530

    new

--- Documents/VSCode programs/codewithharry/apnacollegeprograms/tut6.3/qno3a.cpp
+++ Documents/VSCode programs/codewithharry/apnacollegeprograms/tut6.3/qno3a.cpp
@@ -3,44 +3,44 @@
 using namespace std;
 
 int main(){
-   int a,b,c;
-   cout<<"Enter the number";
-   cin>>a>>b>>c;
+    int a,b,c;
+    cout<<"Enter the number"<<endl;
+    cin>>a>>b>>c;
 
-   int max = a;
-   if(b > max){
-      max = b;
-   }
-   if(c > max){
-      max = c;
-   }
+    int max = a;
+    if(b > max){
+        max = b;
+    }
+    if(c > max){
+        max = c;
+    }
 
-   if (max == a){
-      if(a*a == (b*b + c*c)){
-         cout<<true << endl;
-      }
-   }       
-   else{
-      cout<<"false"<<endl;
-   }
-   elseif( max == b ) {
-      if(b*b == (a*a + c*c)){
-         cout<<true << endl;
-      }
-   }       
-   else{
-      cout<<"false"<<endl;
-   }
+    if (max == a){
+        if(a*a == (b*b + c*c)){
+            cout<<true << endl;
+        }
+    else{
+            cout<<"false"<<endl;
+        }
+    }       
+else if( max == b ) {
+        if(b*b == (a*a + c*c)){
+            cout<<true << endl;
+        }
+    else{
+            cout<<"false"<<endl;
+        }
+    }       
 
-   else if( max == b ) {
-      if(c*c == (a*a + b*b)){
-         cout<<true << endl;
-      }
-   }       
-   else{
-      cout<<"false"<<endl;
-   }
+else if( max == b ) {
+        if(c*c == (a*a + b*b)){
+            cout<<true << endl;
+        }
+    else{
+            cout<<"false"<<endl;
+        }
+    }       
 
-   return 0; 
+    return 0; 
 }
 
