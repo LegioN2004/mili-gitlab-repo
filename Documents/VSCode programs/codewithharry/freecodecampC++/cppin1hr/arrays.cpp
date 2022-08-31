5aa95288e8e1f9d4e53d783b90c567049d4ed08c
LegioN2004 <sunnybaruasins@gmail.com>
2022-08-31 23:04:40 +0530

    shifted all my programs in a new repo

--- Documents/VSCode programs/codewithharry/freecodecampC++/cppin1hr/arrays.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  int numbers[19];
-  numbers[0] = 100;
-  cout << numbers[0];
-  return 0;
-}
--- Documents/VSCode programs/codewithharry/freecodecampC++/cppin1hr/whileloop.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  int index = 1;
-  while () {
-  }
-  return 0;
-}
